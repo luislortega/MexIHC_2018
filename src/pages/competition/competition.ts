636c25cbe5bb705f09f811101c2d199c37754e30
Luis Leon <leon.luisgerardo775@gmail.com>
2018-10-28 19:45:22 -0600

    -Se repararon los PDFs de algunas secciones -Ya se añadieron los iconos para Android

--- src/pages/competition/competition.ts
+++ src/pages/competition/competition.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 /**
  * Generated class for the CompetitionPage page.
@@ -16,18 +17,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class CompetitionPage {
 
   menu_competition = [
-    {'title': 'Kuali: Distribuyendo comida por un bien', 'name':'Julieta Navarro Rivera, Erika Yunuen Torres Torres, Giovanny Alfonso Chávez Ceniceros, & Raymundo Cornejo'},
-    {'title': 'HELPi: Diseño de una herramienta para entregar en el lugar apoyo al indigente', 'name':'Alain Diaz-Montes, Carlos G. Verdin, Carlos A. Puga, Ricardo Rodríguez-Torres, Marcela D. Rodríguez'},
-    {'title': 'HIPO: Aplicacion multiplataforma para el apoyo del aprendizaje de habilidades matematicas dirigida a personas con dispacacidad intelectual', 'name': 'Abies C. Durán L., Mario A. Marín B., Karla N. Rocha R., Julián Cárdenas P., Aída L. González L.'},
-    {'title': 'Kandii Puzzle: Rompecabezas interactivo para apoyar dispacidad intelectual', 'name': 'David May, Diana Arias, Lorena Baez, Miguel Angel Rivera, Rafael Luna'},
-    {'title': 'Tisekuae Xuta Xtaya: Ayuda a las personas sordas', 'name': 'Eduardo Armas García, Rafael Cano Martínez,Víctor Hernández Vásquez, Luis C. Mota Carrera, María del Rosario Peralta Calvo'}
+    {'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Competition/Document_KUALI_MexIHC2018.pdf ','title': 'Kuali: Distribuyendo comida por un bien', 'name':'Julieta Navarro Rivera, Erika Yunuen Torres Torres, Giovanny Alfonso Chávez Ceniceros, & Raymundo Cornejo'},
+    {'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Competition/Helpi.pdf ','title': 'HELPi: Diseño de una herramienta para entregar en el lugar apoyo al indigente', 'name':'Alain Diaz-Montes, Carlos G. Verdin, Carlos A. Puga, Ricardo Rodríguez-Torres, Marcela D. Rodríguez'},
+    {'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Competition/HIPO-Aplicacioìn-observ-final.pdf ','title': 'HIPO: Aplicacion multiplataforma para el apoyo del aprendizaje de habilidades matematicas dirigida a personas con dispacacidad intelectual', 'name': 'Abies C. Durán L., Mario A. Marín B., Karla N. Rocha R., Julián Cárdenas P., Aída L. González L.'},
+    {'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Competition/Kandiipuzzle_rompecabezas interactivo.pdf ','title': 'Kandii Puzzle: Rompecabezas interactivo para apoyar dispacidad intelectual', 'name': 'David May, Diana Arias, Lorena Baez, Miguel Angel Rivera, Rafael Luna'},
+    {'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Competition/Paper_AyudaPersonasSordasCameraReady.pdf ','title': 'Tisekuae Xuta Xtaya: Ayuda a las personas sordas', 'name': 'Eduardo Armas García, Rafael Cano Martínez,Víctor Hernández Vásquez, Luis C. Mota Carrera, María del Rosario Peralta Calvo'}
   ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public ia: InAppBrowser) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CompetitionPage');
   }
+
+  openPDF(urlTxt){
+    let url = encodeURIComponent(urlTxt); 
+    this.ia.create('https://docs.google.com/viewer?url=' + url);
+  }
+
 
 }
--- src/pages/shortpapers/shortpapers.ts
+++ src/pages/shortpapers/shortpapers.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 /**
  * Generated class for the ShortpapersPage page.
@@ -16,17 +17,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class ShortpapersPage {
 
   menu_shortpapers = [
-    {'title': 'A Computer Graphics Program to Make Exercises Using the Kinect Device', 'name':'Francisco Madera Ramirez, Emilio Segura Pool, Jorde Rios Martinez'},
-    {'title': 'A preliminary study using a web camera based eye tracking to assess novelty reaction allowing user interaction', 'name':'Jessica Beltrán, Isaac Ríos Vázquez, Ámbar S. Sánchez Cortez, René F. Navarro, Luis A. Maldonado Cano, Mireya S. García Vázquez'},
-    {'title': 'Códice: Assisting Vocabulary Learning for Students with Deafness', 'name':'Claudia Quiroz Pelayo, JRG Pulido, María Andrade Aréchiga, Silvia Fajardo Flores'},
-    {'title': 'From Touches to Teamwork Constructs: Towards Automatically Visualising Collaboration Processes', 'name':'Adrian Venegas Reynoso, Laura Sanely Gaytán Lugo, Roberto Martinez Maldonado'}
+    {'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Papers/A%20Computer%20Graphics%20Program%20to%20Make%20Exercises%20Using%20the%20%20Kinect%20Device.pdf','title': 'A Computer Graphics Program to Make Exercises Using the Kinect Device', 'name':'Francisco Madera Ramirez, Emilio Segura Pool, Jorde Rios Martinez'},
+    {'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Papers/A preliminary study using a web camera based eye tracking to assess novelty reaction allowing user interaction.pdf','title': 'A preliminary study using a web camera based eye tracking to assess novelty reaction allowing user interaction', 'name':'Jessica Beltrán, Isaac Ríos Vázquez, Ámbar S. Sánchez Cortez, René F. Navarro, Luis A. Maldonado Cano, Mireya S. García Vázquez'},
+    {'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Papers/C%C3%B3dice_%20Assisting%20Vocabulary%20Learning%20for%20Students%20with%20%20Deafness.pdf','title': 'Códice: Assisting Vocabulary Learning for Students with Deafness', 'name':'Claudia Quiroz Pelayo, JRG Pulido, María Andrade Aréchiga, Silvia Fajardo Flores'},
+    {'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Papers/From Touches to Teamwork Constructs_ Towards Automatically Visualising Collaboration Processes.pdf ','title': 'From Touches to Teamwork Constructs: Towards Automatically Visualising Collaboration Processes', 'name':'Adrian Venegas Reynoso, Laura Sanely Gaytán Lugo, Roberto Martinez Maldonado'}
   ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public ia: InAppBrowser) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ShortpapersPage');
+  }
+
+  openPDF(urlTxt){
+    let url = encodeURIComponent(urlTxt); 
+    this.ia.create('https://docs.google.com/viewer?url=' + url);
   }
 
 }
