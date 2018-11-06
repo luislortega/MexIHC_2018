c3c5cb985fda6aad83bfa0841f192b75793c2f46
Luis Leon <leon.luisgerardo775@gmail.com>
2018-11-06 11:00:28 -0600

    Aplicacion terminada.

--- src/pages/october30/october30.ts
+++ src/pages/october30/october30.ts
@@ -17,7 +17,25 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
 export class October30Page {
 
   tabla_30 = [
-    {'time':'22:00','activity': 'Return to merida', 'place':'. . .', 'volunteer': '3 Student Vounteer','url':'none'}
+    {'time':'9:00 - 9:15','activity': 'Registration', 'place':'. . .', 'volunteer': '3 Students Volunteer','url':'none'},
+    {'time':'9:15 - 9:30','activity': 'Chairs welcome', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'none','url':'none'},
+    {'time':'9:30 - 10:45','activity': 'Opening keynote Beverly May, VP of UX for GE Healthcare Digital', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'none','url':'none'},
+    {'time':'10:45 - 11:00','activity': 'BREAK', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'COFEE BREAK starting 14:45, Auditorium Foyer','url':'none'},
+    {'time':'11:00 - 12:45','activity': 'Session 1 Human-computer studies, chair: Jessica Beltrán', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'COFEE BREAK starting 14:45, Auditorium Foyer','url':'none'},
+    {'time':'11:00 - 11:30','activity': 'Modularity is the Key: A New Approach to Social Media Privacy Policies', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': '2 Student Volunteer','url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Papers/Modularity%20is%20the%20Key%20%20A%20New%20Approach%20to%20Social%20Media%20Privacy%20Policies.pdf'},
+    {'time':'11:30 - 12:00','activity': 'Buenas Prácticas de Usabilidad para el Desarrollo de los Sistemas de la Universidad Tecnológica de la Mixteca', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'none','url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Papers/Buenas%20Prácticas%20de%20Usabilidad%20para%20el%20Desarrollo%20de%20los%20Sistemas%20de%20la%20Universidad%20Tecnológica%20de%20la%20Mixteca.pdf '},
+    {'time':'12:00 - 12:30','activity': 'La Inclusión de los Estudiantes a la Innovación Tecnológica: Producción y Comercialización de Proyectos Académicos ', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'none','url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Papers/Including%20Students%20in%20Technological%20Development_%20Production%20and%20Marketing%20of%20Academic%20Projects%20%E2%80%93%20Student%20%20Design%20Competition.pdf'},
+    {'time':'12:30 - 12:45','activity': 'A preliminary study using a web camera based eye tracking to assess novelty reaction allowing user interaction (Short paper)', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'none','url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Papers/A preliminary study using a web camera based eye tracking to assess novelty reaction allowing user interaction.pdf'},
+    {'time':'12:45 - 14:00','activity': 'FOOD', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'none','url':'none'},
+    {'time':'14:00 - 15:15','activity': 'Session 2 Assistive Technologies, chair: Dago Cruz', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': '2 Student Volunteer','url':'none'},
+    {'time':'14:00 - 14:15','activity': 'Códice : Assisting Vocabulary Learning for Students with Deafness (Short paper)', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'none','url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Papers/C%C3%B3dice_%20Assisting%20Vocabulary%20Learning%20for%20Students%20with%20%20Deafness.pdf'},
+    {'time':'14:15 - 14:45','activity': 'Designing Interaction in a Speaking System for the Blind and Low Vision: Case Study', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'none','url': 'http://convivenciaescolaryucatan.com.mx/php/pdfs/Papers/Designing%20Interaction%20in%20a%20Speaking%20System%20for%20the%20Blind%20%20and%20Low%20Vision_%20Case%20Study.pdf'},
+    {'time':'14:45 - 15:15','activity': 'RehWave: A Real-Time Tele-Rehabilitation Haptic Device for Massage Therapy', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'none','url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Papers/RehWave_%20A%20Real-Time%20Tele-Rehabilitation%20Haptic%20Device%20for%20%20Massage%20Therapy.pdf'},
+    {'time':'15:15 - 15:30','activity': 'BREAK', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'COFEE BREAK starting 14:45, Auditorium Foyer','url':'none'},
+    {'time':'15:30 - 16:30','activity': 'Poster session ', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'COFEE BREAK starting 14:45, Auditorium Foyer','url':'none'},
+    {'time':' - - -','activity': 'See http://www.mexihc.org/2018/accepted.html ', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'none','url':'none'},
+    {'time':'16:30 - 17:45','activity': 'Keynote Saiph Savage, Assistant Professor of Computer Science at West Virginia University', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'none','url':'none'},
+    {'time':'18:00 - 20:00','activity': 'Gala Dinner', 'place':'Auditorio "Manuel Cepeda Peraza"', 'volunteer': 'Student Volunteers','url':'none'}
   ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public ia: InAppBrowser) {}
--- src/pages/october31/october31.ts
+++ src/pages/october31/october31.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 /**
  * Generated class for the October31Page page.
@@ -15,11 +16,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class October31Page {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  tabla_31 = [
+    {'time': '10:15 - 10:30', 'activity': 'Opening by SDC Chairs', 'place': 'Auditorio "Manuel Cepeda Peraza"', 'volunteer': '- - -', 'url':'none'},
+    {'time': '10:30 - 10:45', 'activity': '”Kuali: Distribuyendo comida por un bien” Julieta Navarro Rivera,  Erika Yunuen Torres Torres, Giovanny Alfonso Chávez Ceniceros, Raymundo Cornejo', 'place': 'Auditorio "Manuel Cepeda Peraza"', 'volunteer': '- - -', 'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Competition/Document_KUALI_MexIHC2018.pdf '},
+    {'time': '10:45 - 11:00', 'activity': '“HIPO: Aplicación multiplataforma para el apoyo del aprendizaje de habilidades matemáticas dirigida a personas con discapacidad intelectual” Abies C. Durán L., Mario A. Marín B., Karla N. Rocha R.,  Julián Cárdenas P., Aída L. González L.', 'place': 'Auditorio "Manuel Cepeda Peraza"', 'volunteer': '- - -', 'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Competition/HIPO-Aplicacioìn-observ-final.pdf '},
+    {'time': '11:00 - 11:15', 'activity': '“Tisekuae Xuta Xtaya: Ayuda a las Personas Sordas” Eduardo Armas García, Rafael Cano Martínez, Víctor Hernández Vásquez, Luis C. Mota Carrera, María del Rosario Peralta Calvo. ', 'place': 'Auditorio "Manuel Cepeda Peraza"', 'volunteer': '- - -', 'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Competition/Paper_AyudaPersonasSordasCameraReady.pdf '},
+    {'time': '11:15 - 11:30', 'activity': 'BREAK', 'place': 'Auditorio "Manuel Cepeda Peraza"', 'volunteer': '- - -', 'url':'none'},
+    {'time': '11:30 - 12:10', 'activity': 'SDC Chairs Talk', 'place': 'Auditorio "Manuel Cepeda Peraza"', 'volunteer': '- - -', 'url':'none'},
+    {'time': '12:10 - 12:25', 'activity': '“Diseño de una Herramienta para Entregar en el Lugar aPoyo al indigente: HELPi” Alain Diaz-Montes, Carlos G. Verdin, Carlos A, Puga, Ricardo Rodríguez-Torres, Marcela D. Rodríguez. ', 'place': 'Auditorio "Manuel Cepeda Peraza"', 'volunteer': '- - -', 'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Competition/Helpi.pdf '},
+    {'time': '12:25 - 12:40', 'activity': '“Kandii Puzzle: Rompecabezas interactivo para apoyar la discapacidad intelectual” David May, Diana Arias, Lorena Baez, Miguel Rivera, Rafael Luna.  ', 'place': 'Auditorio "Manuel Cepeda Peraza"', 'volunteer': '- - -', 'url':'http://convivenciaescolaryucatan.com.mx/php/pdfs/Competition/Kandiipuzzle_rompecabezas interactivo.pdf '},
+    {'time': '12:40 - 12:55', 'activity': 'Select the winner', 'place': 'Auditorio "Manuel Cepeda Peraza"', 'volunteer': '- - -', 'url':'none'},
+    {'time': '12:55 - 13:05', 'activity': 'Awards and closing', 'place': 'Auditorio "Manuel Cepeda Peraza"', 'volunteer': '- - -', 'url':'none'}
+  ]
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public ia: InAppBrowser) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad October31Page');
+  }
+
+  openPDF(urlTxt){
+    let url = encodeURIComponent(urlTxt); 
+    this.ia.create('https://docs.google.com/viewer?url=' + url);
   }
 
 }
