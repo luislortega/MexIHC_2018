58a66f61195ecb64e418a338d5d5dba1d36fbb37
Reynaldo Salazar <reysalazar1213@gmail.com>
2018-11-11 01:50:22 -0600

    Cambios Visuales y About Page
    Cambios en el diseño y splashscreen puesto. Tambien se añadio lo del about page.

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -7,6 +7,12 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
+  speakers = [
+    {name: "Saiph Savage", img: "http://www.mexihc.org/2018/images/saiph.jpg"},
+    {name: "Beverly May", img: "http://www.mexihc.org/2018/images/beverly.jpg"},
+    {name: "Luis Alberto Muñoz Ubando", img: "http://www.mexihc.org/2018/images/alberto.jpeg"}
+  ]
+
   constructor(public navCtrl: NavController) {
 
   }
